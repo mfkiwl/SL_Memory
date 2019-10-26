6a5028cd1afd37c0468e8d2688d5466bf085bbec
Leon Shan <813475603@qq.com>
2019-10-26 22:16:28 +0800

    NEW: Init source files

---
+++ RTE_Memory.c
@@ -0,0 +1,30 @@
+/**
+ * @file RTE_Memory.c
+ * @author Leon Shan (813475603@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-26
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#include "RTE_Memory.h"
+#include <stdio.h>
+
+
+
+
+
+#if RTE_MEMORY_TEST
+/**
+ * @brief A general main function for test.
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char *argv[]){
+    printf("Helloworld!");
+    return 0;
+}
+#endif
---
+++ RTE_Memory.h
@@ -0,0 +1,18 @@
+/**
+ * @file RTE_Memory.h
+ * @author Leon Shan (813475603@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-26
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#ifndef __RET_MEMORY_H
+#define __RET_MEMORY_H
+#include <stdint.h>
+#define RTE_MEMORY_TEST     1           /* define for if enable test main */
+
+
+
+#endif
